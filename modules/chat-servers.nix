a82695a13cb3babdcc7d32279671fc3e3e3fc5ff
Kerry Cerqueira <kerrycerqueira@gmail.com>
2025-11-27 17:25:14 -0500

    Add writable data dir to openwebui derivation

--- modules/chat-servers.nix
+++ modules/chat-servers.nix
@@ -1,24 +1,31 @@
 {inputs, ...}: {
-  perSystem = {system, lib, ...}: let
+  perSystem = {
+    system,
+    lib,
+    ...
+  }: let
     pkgs = import inputs.nixpkgs {
       inherit system;
       config.allowUnfree = true;
     };
   in {
-    packages.openwebui = {
-      program = pkgs.writeShellApplication {
-        name = "openwebui-server";
-        runtimeEnv = {
-          ANONYMIZED_TELEMETRY = "False";
-          DO_NOT_TRACK = "True";
-          SCARF_NO_ANALYTICS = "True";
-          WEBUI_AUTH = "False";
-        };
-        text = ''
-          exec ${lib.getExe pkgs.open-webui} serve \
-          --host 0.0.0.0 --port 8082 "$@"
-        '';
+    packages.openwebui = pkgs.writeShellApplication {
+      name = "openwebui-server";
+      runtimeEnv = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        WEBUI_AUTH = "False";
       };
+      text = ''
+        : "''${XDG_DATA_HOME:="$HOME/.local/share"}"
+        : "''${DATA_DIR:="$XDG_DATA_HOME/open-webui"}"
+        mkdir -p "''${DATA_DIR}"
+        export DATA_DIR
+
+        exec ${lib.getExe pkgs.open-webui} serve \
+        --host 0.0.0.0 --port 8082 "$@"
+      '';
     };
   };
 }
