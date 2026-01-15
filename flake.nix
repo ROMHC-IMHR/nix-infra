f46c5341f66f392074b0cd976d3592745c9fb794
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-01-14 23:58:50 -0500

    Pin stable releases, update lock

--- flake.nix
+++ flake.nix
@@ -3,8 +3,19 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-    kc-nix-infra.url = "github:KerryCerqueira/nix-infra";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    kc-nix-infra = {
+      url = "github:KerryCerqueira/nix-infra";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     expy = {
       url = "github:KerryCerqueira/expy";
       inputs.nixpkgs.follows = "nixpkgs";
