9aa15c9cdf4861239832fcecc97dc7aa3c4483c3
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-01-19 13:58:12 -0500

    Putative network config for muncher

--- modules/hosts/muncher/networking.nix
+++ modules/hosts/muncher/networking.nix
@@ -1,18 +1,24 @@
 {
   flake.nixosModules.muncher = {
-    # 1. Disable DHCP for this interface (replace 'enp3s0' with your actual interface name!)
-    # networking.interfaces.enp3s0.useDHCP = false;
-
-    # 2. Hardcode the IP and Subnet
-    # networking.interfaces.enp3s0.ipv4.addresses = [ {
-    #   address = "192.168.1.50"; # The IP you want
-    #   prefixLength = 24;        # This means subnet mask 255.255.255.0
-    # } ];
-
-    # 3. Hardcode the Router's IP (Gateway)
-    # networking.defaultGateway = "192.168.1.1";
-
-    # 4. Hardcode DNS (Since the router isn't telling us anymore)
-    # networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    networking = {
+      hostName = "muncher";
+      useDHCP = false;
+      interfaces = {
+        eno8303 = {
+          ipv4.addresses = [
+            {
+              address = "10.156.156.172";
+              prefixLength = 24;
+            }
+          ];
+        };
+        eno8403 = {
+          useDHCP = false;
+        };
+      };
+      defaultGateway = "10.156.156.1";
+      nameservers = ["10.156.156.10" "10.156.156.11"];
+      search = ["bic.theroyal.ca"];
+    };
   };
 }
