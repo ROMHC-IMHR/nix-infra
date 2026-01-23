d4b3a49b3548d534771ff45a91391483fa8740dc
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-01-23 19:11:01 +0000

    Add ktabay user

---
+++ modules/hosts/muncher/users/ktabay.nix
@@ -0,0 +1,47 @@
+{self, ...}: let
+  username = "ktabay";
+  fullname = "Konrad Tabay";
+  sshKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSimJGwX8BKJmip04XakVzcjl39tD09vNICeIK5lO0S"
+  ];
+in {
+  flake.nixosModules.muncher = {
+    config,
+    pkgs,
+    ...
+  }: let
+    hddPath = "/mnt/hddstore/users/${username}";
+    hddHomePath = "${config.users.users.ktabay.home}/hdd";
+    ssdPath = "mnt/ssdstore/users/${username}";
+    ssdHomePath = "${config.users.users.ktabay.home}/ssd";
+  in {
+    users.users."${username}" = {
+      isNormalUser = true;
+      description = fullname;
+      uid = 1001;
+      openssh.authorizedKeys.keys = sshKeys;
+      packages = with pkgs; [
+        uv
+      ];
+    };
+    home-manager.users."${username}" = {
+      imports = [self.homeModules."${username}"];
+    };
+    fileSystems."${ssdHomePath}" = {
+      device = ssdPath;
+      options = ["bind"];
+      depends = ["/mnt/ssdstore"];
+    };
+    fileSystems."${hddHomePath}" = {
+      device = hddPath;
+      options = ["bind"];
+      depends = ["/mnt/hddstore"];
+    };
+    systemd.tmpfiles.rules = [
+      "d ${ssdPath} 0700 ${username} users -"
+      "d ${ssdHomePath} 0700 ${username} users -"
+      "d ${hddPath} 0700 ${username} users -"
+      "d ${hddHomePath} 0700 ${username} users -"
+    ];
+  };
+}
