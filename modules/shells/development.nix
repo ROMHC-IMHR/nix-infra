3ddbb842227a1ff96a64d27d3043f24e81ab6206
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-04-22 15:26:56 -0400

    Fix misnamed devshell attr

--- modules/shells/development.nix
+++ modules/shells/development.nix
@@ -2,7 +2,7 @@
   perSystem = {inputs', pkgs, ...}: {
     devShells.default = pkgs.mkShell {
       packages = [
-        (inputs'.packages.neovim.wrap {
+        (inputs'.kc-nix-infra.packages.neovim.wrap {
           aspects.lang.nix.enable = true;
         })
       ];
