87c82037808f45bb218f4b5182280cdb74d4e6fb
Kerry Cerqueira <kerrycerqueira@gmail.com>
2026-06-08 15:30:11 -0400

    Format filesystem attrset

--- modules/hosts/muncher/filesystem.nix
+++ modules/hosts/muncher/filesystem.nix
@@ -7,34 +7,36 @@
       algorithm = "zstd";
       memoryPercent = 25;
     };
-    fileSystems."/" = {
-      label = "mncr-root";
-      fsType = "ext4";
-    };
-    fileSystems."/boot" = {
-      label = "MNCR-BOOT";
-      fsType = "vfat";
-      options = ["fmask=0077" "dmask=0077"];
-    };
-    fileSystems."/home" = {
-      label = "mncr-home";
-      fsType = "ext4";
-    };
-    fileSystems."/nix" = {
-      label = "mncr-nix";
-      fsType = "ext4";
-    };
-    fileSystems."/var" = {
-      label = "mncr-var";
-      fsType = "ext4";
-    };
-    fileSystems."/mnt/ssdstore" = {
-      label = "SSD_2TB";
-      fsType = "ext4";
-    };
-    fileSystems."/mnt/hddstore" = {
-      label = "HDD_12TB";
-      fsType = "ext4";
+    fileSystems = {
+      "/" = {
+        label = "mncr-root";
+        fsType = "ext4";
+      };
+      "/boot" = {
+        label = "MNCR-BOOT";
+        fsType = "vfat";
+        options = ["fmask=0077" "dmask=0077"];
+      };
+      "/home" = {
+        label = "mncr-home";
+        fsType = "ext4";
+      };
+      "/nix" = {
+        label = "mncr-nix";
+        fsType = "ext4";
+      };
+      "/var" = {
+        label = "mncr-var";
+        fsType = "ext4";
+      };
+      "/mnt/ssdstore" = {
+        label = "SSD_2TB";
+        fsType = "ext4";
+      };
+      "/mnt/hddstore" = {
+        label = "HDD_12TB";
+        fsType = "ext4";
+      };
     };
     systemd.tmpfiles.rules = [
       "d /mnt/ssdstore 0755 root root -"
